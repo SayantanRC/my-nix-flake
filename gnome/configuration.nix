ad510b3c19572e2705a8ab321f03e5b838cdf489
sayantan <SayantanRC>
2023-09-24 07:54:51 +0530

    move gnome desktop lines

--- gnome/configuration.nix
+++ gnome/configuration.nix
@@ -1,5 +1,10 @@
 { config, pkgs, lib, ... }:
 {
+
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+  
   environment.systemPackages = with pkgs; [
     cinnamon.nemo-with-extensions
     papirus-icon-theme
