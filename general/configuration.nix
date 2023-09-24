c762ebc2d58610b7804a4f17315ff1981ae11495
sayantan <SayantanRC>
2023-09-24 18:42:19 +0530

    try manually setting xdg dirs

--- general/configuration.nix
+++ general/configuration.nix
@@ -136,5 +136,16 @@
     formatted = builtins.concatStringsSep "\n" sortedUnique;
   in
     formatted;
+    
+  environment.etc."xdg/user-dirs.defaults".text = ''
+    DESKTOP=Desktop
+    DOCUMENTS=Documents
+    DOWNLOAD=Downloads
+    MUSIC=Music
+    PICTURES=Pictures
+    PUBLICSHARE=Public
+    TEMPLATES=Templates
+    VIDEOS=Videos
+  '';
 
 }
