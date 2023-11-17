4cc578881e04448e5a6e4ae812372dc96bda209b
sayantan <SayantanRC>
2023-11-18 02:36:06 +0530

    add default nvidia options

--- hardware/asus_x13.nix
+++ hardware/asus_x13.nix
@@ -44,4 +44,26 @@
       enableUserService = true;
     };
   };
+  
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  };
+
+  services.xserver.videoDrivers = ["nvidia"];
+
+  hardware.nvidia = {
+
+    modesetting.enable = true;
+
+    powerManagement.enable = false;
+    powerManagement.finegrained = false;
+
+    open = false;
+
+    nvidiaSettings = true;
+
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
 }
