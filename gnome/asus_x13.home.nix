6b29f7cd0a440ff3da1e10dc765845a9547f1540
sayantan <SayantanRC>
2023-11-18 05:46:04 +0530

    add performance profile toggle shortcut

--- gnome/asus_x13.home.nix
+++ gnome/asus_x13.home.nix
@@ -6,6 +6,7 @@
     "org/gnome/settings-daemon/plugins/media-keys" = {
       custom-keybindings= [
         "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom99/"
+        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom100/"
       ];
     };
 
@@ -13,6 +14,12 @@
       binding="XF86Launch1";
       command="rog-control-center";
       name="ROG control center";
+    };
+
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom100" = {
+      binding="F5";
+      command="asusctl profile -n";
+      name="Performance profile toggle";
     };
 
   };  
