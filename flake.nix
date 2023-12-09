fead42986beb9932fa21ae92734d627d116a92ea
SayantanRC <bin.sayantan@gmail.com>
2023-12-10 02:58:29 +0530

    nixpkgs - use nixos-23.05 instead of release-23.05

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
   
   inputs = {
     nixpkgs = { 
-      url = "github:NixOS/nixpkgs/release-23.05";
+      url = "github:NixOS/nixpkgs/nixos-23.05";
     };
     home-manager = {
       url = "github:nix-community/home-manager/release-23.05";
