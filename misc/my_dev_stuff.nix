789bb72a12a68083079eb1c9bdfdb94cc46536e9
SayantanRC <bin.sayantan@gmail.com>
2023-12-20 07:36:47 +0530

    enable lxd

--- misc/my_dev_stuff.nix
+++ misc/my_dev_stuff.nix
@@ -23,4 +23,21 @@
   environment.sessionVariables = {
     JAVA_HOME = "/home/${username}/android-studio/jbr";
   };
+
+  # https://srid.ca/lxc-nixos
+  # https://discourse.nixos.org/t/howto-setup-lxd-on-nixos-with-nixos-guest-using-unmanaged-bridge-network-interface/21591
+  virtualisation.lxd = {
+    enable = true;
+    ui.enable = true;
+    recommendedSysctlSettings = true;
+  };
+  virtualisation.lxc = {
+    lxcfs.enable = true;
+  };
+  users.users.${username} = {
+    extraGroups = [ "lxd" ];
+  };
+  networking.firewall.extraCommands = ''
+    iptables -A INPUT -i lxdbr0 -m comment --comment "my rule for LXD network lxdbr0" -j ACCEPT
+  '';
 }
