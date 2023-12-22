7e62510aa09a0b8a32b6a2e89bf2317b26ae033e
SayantanRC <bin.sayantan@gmail.com>
2023-12-22 05:41:46 +0530

    improve waydroid

--- misc/waydroid.nix
+++ misc/waydroid.nix
@@ -1,5 +1,12 @@
 { config, pkgs, username, ... }:
 
 {
+  environment.systemPackages = with pkgs; [
+    wl-clipboard
+  ];
   virtualisation.waydroid.enable = true;
+  # sudo waydroid init -s GAPPS -f
+  # sudo systemctl start waydroid-container
+  #
+  # For gplay registration, follow this: https://github.com/waydroid/waydroid/issues/379#issuecomment-1152526650
 }
