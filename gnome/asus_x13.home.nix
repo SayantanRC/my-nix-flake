fa9e9a882341dc8476616d9c9c8a6d84afe978cc
SayantanRC <bin.sayantan@gmail.com>
2024-01-06 04:45:42 +0530

    shortcut for zero fan speed

--- gnome/asus_x13.home.nix
+++ gnome/asus_x13.home.nix
@@ -6,21 +6,28 @@
    
     "org/gnome/settings-daemon/plugins/media-keys" = {
       custom-keybindings= [
-        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom99/"
         "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom100/"
+        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom101/"
+        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom102/"
       ];
     };
 
-    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom99" = {
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom100" = {
       binding="XF86Launch1";
       command="rog-control-center";
       name="ROG control center";
     };
 
-    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom100" = {
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom101" = {
       binding="XF86Launch4";
       command="fan-speed-toggle";
       name="Fan speed toggle";
+    };
+
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom102" = {
+      binding="<Shift>XF86Launch4";
+      command="fan-speed-toggle zero";
+      name="Zero fan speed";
     };
 
   };  
