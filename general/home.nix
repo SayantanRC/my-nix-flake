be9854c7359878a9b1f769773f0c8a64b4b239b5
SayantanRC <bin.sayantan@gmail.com>
2024-01-09 15:57:27 +0530

    change how user-dirs.dirs is made

--- general/home.nix
+++ general/home.nix
@@ -13,17 +13,31 @@ in {
   # https://forum.manjaro.org/t/please-help-all-files-in-home-name-immediately-appear-on-desktop/28173/2
   # https://wiki.archlinux.org/title/XDG_user_directories
   # https://github.com/NixOS/nixpkgs/issues/221604
-  home.file.".config/user-dirs.dirs" = {
-    text = ''
-      XDG_DESKTOP_DIR="$HOME/Desktop"
-      XDG_DOCUMENTS_DIR="$HOME/Documents"
-      XDG_DOWNLOAD_DIR="$HOME/Downloads"
-      XDG_MUSIC_DIR="$HOME/Music"
-      XDG_PICTURES_DIR="$HOME/Pictures"
-      XDG_PUBLICSHARE_DIR="$HOME/Public"
-      XDG_TEMPLATES_DIR="$HOME/Templates"
-      XDG_VIDEOS_DIR="$HOME/Videos"
-    '';
+  #home.file.".config/user-dirs.dirs" = {
+  #  force = true;
+  #  text = ''
+  #    XDG_DESKTOP_DIR="$HOME/Desktop"
+  #    XDG_DOCUMENTS_DIR="$HOME/Documents"
+  #    XDG_DOWNLOAD_DIR="$HOME/Downloads"
+  #    XDG_MUSIC_DIR="$HOME/Music"
+  #    XDG_PICTURES_DIR="$HOME/Pictures"
+  #    XDG_PUBLICSHARE_DIR="$HOME/Public"
+  #    XDG_TEMPLATES_DIR="$HOME/Templates"
+  #    XDG_VIDEOS_DIR="$HOME/Videos"
+  #  '';
+  #};
+
+  xdg.userDirs = {
+    enable = true;
+    createDirectories = true;
+    desktop = "${config.home.homeDirectory}/Desktop";
+    documents = "${config.home.homeDirectory}/Documents";
+    download = "${config.home.homeDirectory}/Downloads";
+    music = "${config.home.homeDirectory}/Music";
+    pictures = "${config.home.homeDirectory}/Pictures";
+    publicShare = "${config.home.homeDirectory}/Public";
+    templates = "${config.home.homeDirectory}/Templates";
+    videos = "${config.home.homeDirectory}/Videos";
   };
   
   home.packages = with pkgs; [
