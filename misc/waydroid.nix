07ca8b096e61d5fa17d1409021d7cd5b4f0e7670
SayantanRC <bin.sayantan@gmail.com>
2024-01-10 14:27:25 +0530

    comments for waydroid

--- misc/waydroid.nix
+++ misc/waydroid.nix
@@ -10,4 +10,8 @@
   #
   # For gplay registration, follow this: https://github.com/waydroid/waydroid/issues/379#issuecomment-1152526650
   # Or here: https://docs.waydro.id/faq/google-play-certification
+
+  # edit /var/lib/waydroid/waydroid_base.prop
+  # ro.hardware.gralloc=default
+  # ro.hardware.egl=swiftshader
 }
