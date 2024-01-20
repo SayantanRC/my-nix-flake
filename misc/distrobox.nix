c3fdbb2eb92a8b368f0a136710c3896c90ecb372
SayantanRC <bin.sayantan@gmail.com>
2024-01-20 19:12:29 +0530

    distrobox commands

--- misc/distrobox.nix
+++ misc/distrobox.nix
@@ -9,4 +9,12 @@
   environment.systemPackages = with pkgs; [
     distrobox
   ];
+
+  # Distrobox commands
+  # Create:           distrobox-create --name Debian-latest-1 --image debian:latest
+  # List:             distrobox-list
+  # Enter:            distrobox-enter --name Debian-latest-1
+  # Export (inside):  distrobox-export --app flameshot
+  # Stop:             distrobox-stop Debian-latest-1
+  # Delete:           distrobox-rm Debian-latest-1
 }
