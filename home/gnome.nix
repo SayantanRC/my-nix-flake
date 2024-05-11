c1dd85b2d773294a967415bb9c828caeb337c74b
SayantanRC <bin.sayantan@gmail.com>
2024-05-12 00:17:50 +0530

    gnome: add calculator shortcut

--- home/gnome.nix
+++ home/gnome.nix
@@ -119,6 +119,7 @@
    
     "org/gnome/settings-daemon/plugins/media-keys" = {
       home = ["<Super>e"];
+      calculator = ["<Super>c"];
       custom-keybindings= [
         "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
         "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/"
