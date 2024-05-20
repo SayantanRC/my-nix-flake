308b808f0dc6219441941747ebd46df8a99f0cdd
SayantanRC <bin.sayantan@gmail.com>
2024-05-21 00:52:05 +0530

    libvert - add more documentation

--- misc/libvert.nix
+++ misc/libvert.nix
@@ -1,11 +1,22 @@
 # https://github.com/erictossell/nixflakes/blob/main/modules/virt/libvirt.nix
 # https://www.reddit.com/r/NixOS/comments/177wcyi/best_way_to_run_a_vm_on_nixos/
 
-# Share directory:
-# https://www.debugpoint.com/share-folder-virt-manager/
+# WINDOWS Stuff
+#
+# 1. HOST - Set "Video" to "Virtio".
+# 2. GUEST - Install VirtIO drivers after mounting ISO:
+# https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/
+# 3. GUEST - Install spice guest tools:
+# https://www.spice-space.org/download.html#windows-binaries
+# 4. HOST - Enable 3D Acceleration:
+# https://discourse.nixos.org/t/opengl-can-not-be-used-with-libvirt/43688/6
+# 5. Share a folder:
 # https://www.debugpoint.com/kvm-share-folder-windows-guest/
-# Virtiofsd:
+# Virtiofsd (may be optional):
 # https://discourse.nixos.org/t/virt-manager-cannot-find-virtiofsd/26752
+
+# More on share directory:
+# https://www.debugpoint.com/share-folder-virt-manager/
 
 { pkgs, username, home-manager, ... }:
 {
